d5b9bad70fcb83d5a3a4561cd11275457f82d7b4
leedeokmu <deolee@gmarket.com>
2023-01-23 00:43:48 +0900

    feat : add r

---
+++ redis-spring2/src/main/java/com/freeefly/redisspring2/RedisSpring2Application.java
@@ -0,0 +1,13 @@
+package com.freeefly.redisspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpring2Application.class, args);
+    }
+
+}
---
+++ redis-spring2/src/test/java/com/freeefly/redisspring2/RedisSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeefly.redisspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
